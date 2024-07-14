092b44b031281047982a3721d1407bb37518311b
Nastazia Coronado <nascoron@gmail.com>
2024-07-13 23:23:34 -0700

    Changed dropdown so it's shown on click.

---
+++ index.js
@@ -0,0 +1,17 @@
+function projectDropdown() {
+    document.getElementById("dropdown-content").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('#dropdown-button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-option");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
