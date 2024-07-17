7358ccd5ea863b77869d8612788481d3bf9b76c5
Nastazia Coronado <nascoron@gmail.com>
2024-07-17 09:01:54 -0700

    Implemented a progress bar function.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const contact = document.getElementById("contact");
 const intro = document.getElementById("intro");
 const projects = document.getElementById("projects");
-const progressCircles = document.querySelectorAll(".front-circle");
+const progressCircles = document.querySelectorAll(".skills-item");
 
 function projectDropdown() {
     document.getElementById("dropdown-content").classList.toggle("show");
@@ -42,7 +42,9 @@ const options = {
 const callbacks = (progressCircles) => {
     progressCircles.forEach(entry => {
         if (entry.isIntersecting){
-            entry.target.classList.add('in-view');
+            var circleDiv = entry.target.childNodes[1].childNodes[3];
+            var percentDiv = entry.target.childNodes[3];
+            progressBar(circleDiv, percentDiv);
         }
     });
 }
@@ -52,3 +54,18 @@ let observer = new IntersectionObserver(callbacks, options);
 progressCircles.forEach(element => {
     observer.observe(element);
 });
+
+function progressBar (circleDiv, percentDiv) {
+    var strokeVal = (3.2 * 100) /  100;
+    var progressVal = percentDiv.dataset.progress;
+    circleDiv.style.strokeDasharray = progressVal * (strokeVal) + ' 999';
+    percentDiv.getAttribute("data-progress", progressVal);
+    var start = new Date().getTime();
+
+    setTimeout(function() {
+        var now = (new Date().getTime()) - start;
+        var progress = now / 700;
+        percentDiv.innerHTML = progressVal / 100 * 100 + '%';
+        if (progress < 1) setTimeout(arguments.callee, 10);
+    }, 10);
+}
